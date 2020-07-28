8c1cf57a9bc7c726d940a7d04213b16a0022ec2e
Jimmy Figueroa <av_jfigueroa@idcontractors.net>
2020-07-28 15:57:18 -0600

    Support for R3 feature completed and test passed

--- lib/salutation.py
+++ lib/salutation.py
@@ -1,4 +1,7 @@
 class salutation(object):
-    def hello_world(self):
-      return {"status": 200, "message": "Success", "payload": "Hello World"}
+    def hello_world(self, param=None):
+      payload = "Hello World"
+      if param is not None:
+         payload = "Hello World to you {} !".format(param)
+      return {"status": 200, "message": "Success", "payload": payload}
 
