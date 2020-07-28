47850f41fd491d40b9d383d6b6af90891acf85e4
Jimmy Figueroa <av_jfigueroa@idcontractors.net>
2020-07-28 15:54:57 -0600

    R3 unit test completed

--- test/test_helloworld.py
+++ test/test_helloworld.py
@@ -17,3 +17,20 @@ def test_display_hw():
        logger.debug("ERROR: exception thrown {}".format(resp["message"]))
 
     assert  "hello world" in resp["payload"].lower()
+
+@pytest.mark.test_param
+def test_hw_with_parameter():
+    logger = logging.getLogger("helloworld")
+    hw = salutation()
+    param = "Susan"
+    resp = hw.hello_world(param)
+
+    # TODO: refactor this with dynamic schema validation: {"status": 200, "message": "", "payload": ""} 
+    # TODO: refactor to extract err messages into files
+    if "status" not in resp or "message" not in resp or "payload" not in resp:
+       logger.debug("ERROR: returning message not in expected format")
+
+    if resp["status"] != 200:
+       logger.debug("ERROR: exception thrown {}".format(resp["message"]))
+
+    assert  "hello world to you {}".format(param.lower()) in resp["payload"].lower()
